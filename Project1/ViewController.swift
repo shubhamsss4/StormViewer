5bc89051e5f43974ee5767890c5bff44318ccadd
shubham shah <shubhamshekharshah@gmail.com>
2020-03-21 20:32:57 +0530

    GCD changes

--- Project1/ViewController.swift
+++ Project1/ViewController.swift
@@ -18,18 +18,24 @@ class ViewController: UITableViewController {
         title = "Storm Viewer"
         navigationController?.navigationBar.prefersLargeTitles = true
         
-        let fm = FileManager.default
-        let path = Bundle.main.resourcePath!
-        let items = try! fm.contentsOfDirectory(atPath: path)
-        
-        for item in items {
-            if item.hasPrefix("nssl"){
-                // this is a picture to load!
-                pictures.append(item)
+        DispatchQueue.global(qos: .userInitiated).async { [weak self] in
+            let fm = FileManager.default
+            let path = Bundle.main.resourcePath!
+            let items = try! fm.contentsOfDirectory(atPath: path)
+            
+            for item in items {
+                if item.hasPrefix("nssl"){
+                    // this is a picture to load!
+                    self?.pictures.append(item)
+                }
+            }
+            self?.pictures = self?.pictures.sorted() ?? []
+            print(self?.pictures ?? "")
+            
+            DispatchQueue.main.async { [weak self] in
+                self?.tableView.reloadData()
             }
         }
-        pictures = pictures.sorted()
-        print(pictures)
     }
 
     
